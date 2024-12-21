4335f00592f5d33fad3650962aa024ca1b4582c8
IEMDomain04.dc <emmanman0011@gmail.com>
2024-12-21 20:35:56 +0800

    First section of aboutpage TODO: Code second section of aboutpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Navbar from "@/components/Navbar";
 import Homepage from "@/components/Home";
 import Image from "next/image";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <>
       <div className="bg-orange-50 overflow-x-hidden">
         <Navbar />
-        <Homepage />
+        <About />
       </div>
     </>
   );
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,13 @@
 export default function About() {
     return (
         <section>
-            <section>
-                <div>
-                    <h1></h1>
-                    <h3></h3>
-                    <button></button>
+            <section className="flex px-20 py-10 items-center">
+                <div className="space-y-8">
+                    <h1 className="text-5xl font-bold tracking-wide max-sm:text-3xl">"Where is this place po?"</h1>
+                    <p className="w-11/12 max-sm:text-xs max-sm:w-10/12">It’s common for students, roamers, and tourists exploring Intramuros to feel a bit overwhelmed or tired when they can’t easily find the places they’re looking for. That’s why this app is here—to make your journey stress-free and help you effortlessly discover the spots you want to visit inside Intramuros.</p>
+                    <button className="py-3 px-9 text-white rounded cursor-pointer duration-300 bg-orange-600 hover:bg-orange-800 max-sm:" type="button">See places</button>
                 </div>
-                <img src="" alt="" />
+                <img src="https://pinasculture.com/wp-content/uploads/2024/05/shutterstock_1906890529-2.webp" width={600} height={400} alt="" />
             </section>
 
             <section>
@@ -17,8 +17,8 @@ export default function About() {
                 </div>
                 
                 <div>
-                    <img src="" alt="" />
-                    <img src="" alt="" />
+                    
+                    
                 </div>
             </section>
         </section>
